30ef0360c6f0275fff0ddcea5907bf8a7cf1ffd2
shaik57 <141896794+shaik57@users.noreply.github.com>
2023-09-11 12:27:21 +0530

    Create hcf.c

---
+++ hcf.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+	int i,j,n,s,c;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		for(s=0;s<n-i-1;s++)
+		printf(" ");
+		for(j=0;j<i+1;j++)
+		{
+			if((i==0)||(j==0))
+			c=1;
+			else
+			c=c*(i-j+1)/j;
+			printf(" %d",c);
+	}
+	printf("\n");
+}
+return 0;
+}
