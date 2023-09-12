8b0d7bbb5af42e73c8971c2559fbaf04f5113fe1
shaik57 <141896794+shaik57@users.noreply.github.com>
2023-09-12 09:01:58 +0530

    Add files via upload

---
+++ dd.cpp
@@ -0,0 +1,19 @@
+ 
+#include<stdio.h>
+
+int main()
+{
+    int num;
+    
+    scanf("%d",&num);
+    
+    while(num > 0) 
+    {
+        int mod = num % 10; 
+        printf("%d\n",mod); 
+    
+        num = num / 10;   
+    }
+    
+    return 0;
+}
---
+++ oddeven.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+	int num;
+	printf("enter the number");
+	scanf("%d",&num);
+	if (num%2==0)
+	printf("it is an even");
+	else
+	printf("it is an odd");
+}
